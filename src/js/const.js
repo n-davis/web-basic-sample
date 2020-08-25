61c094ca6235b89f6e4588b3e170dd36099b3766
Noah Davis <contact@noah.fyi>
2020-08-25 09:30:04 +0900

    Initial commit

---
+++ src/js/const.js
@@ -0,0 +1,14 @@
+export const APP_ID = '3598AA8B-F86D-418C-BAAD-B69C72774FB3';
+export const USER_ID = 'user_id';
+export const DISPLAY_NONE = 'none';
+export const DISPLAY_BLOCK = 'block';
+export const DISPLAY_FLEX = 'flex';
+export const ACTIVE_CLASSNAME = 'active';
+export const KEY_ENTER = 13;
+export const FILE_ID = 'attach_file_id';
+export const UPDATE_INTERVAL_TIME = 5 * 1000;
+export const COLOR_RED = '#DC5960';
+export const MESSAGE_REQ_ID = 'reqId';
+export const OPEN_CHANNEL_SEARCH_URL = 'search_open_channel';
+
+export const body = document.querySelector('body');
